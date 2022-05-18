7e90a489e7aa9cb5c89aa52ab17aa2117e29de68
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-05-18 20:53:56 -0300

    Añade componente 'presentacion' con estilos

---
+++ src/Components/Presentation/Presentation.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const Presentation = () => {
+  return (
+    <section className="about-me">
+      <div className="info-container">
+        <h2 className="about-us">Sobre nosotros</h2>
+        <h3 className="slogan">Tu lugar, tu casa, tu hogar</h3>
+        <p className="paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem dicta rem hic totam tempora animi error perferendis.
+          Vero corrupti porro quia! Ad saepe alias, officiis voluptatem sed,
+          odio dolores neque dolorem placeat nam quia numquam soluta ipsam
+          nostrum consequuntur, a magnam non. Tenetur repudiandae distinctio
+          inventore voluptate fugit laborum?
+        </p>
+        <p className="paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem dicta rem hic totam tempora animi error perferendis.
+          Vero corrupti porro quia! Ad saepe alias, officiis voluptatem sed,
+          odio dolores neque dolorem placeat nam quia numquam soluta ipsam
+          nostrum consequuntur, a magnam non. Tenetur repudiandae distinctio
+          inventore voluptate fugit laborum?
+        </p>
+        <button className="boton">Noticias</button>
+      </div>
+    </section>
+  );
+};
