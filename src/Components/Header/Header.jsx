12269c419371d1c621a68bc56e9fe04807dccfb4
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-05-18 20:35:22 -0300

    Añade carpeta donde estaran los componentes usados, se crea y realiza el componente header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header>
+            <div className='info-header'>
+                <h1 className="title">IG Arquitectos</h1>
+                <h3 className="subtitle">Nuevas ideas en construcción</h3>
+                <button className='boton'>Suscribite</button>
+            </div>
+            <div className="img-header"></div>
+        </header>
+    )
+}
