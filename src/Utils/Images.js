8183b8db53343e4f42e2d1ce8108033e8b7c2706
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-05-18 21:41:34 -0300

    añade carpeta utils, que contiene images para exportar imagenes

---
+++ src/Utils/Images.js
@@ -0,0 +1,9 @@
+import img_01 from '../img/1.jpg'
+import img_03 from '../img/3.png'
+import img_04 from '../img/4.png'
+import img_05 from '../img/5.png'
+
+
+export {
+    img_01, img_03, img_04, img_05
+}
