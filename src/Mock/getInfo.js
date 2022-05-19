b1a8bed6ddba7ddc064a2288ef240a342cad7c7f
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-05-18 22:55:50 -0300

    Añade getinfo, para mostrar datos del equipo

---
+++ src/Mock/getInfo.js
@@ -0,0 +1,20 @@
+export const team =[
+    {
+        id:1,
+        name:"Alberto Gomez",
+        info:'Lorem ipsum dolor sit',
+        img:'arqu2.nng'
+    },
+    {
+        id:2,
+        name:"Analía Jumenez",
+        info:'Lorem ipsum dolor sit',
+        img:'arqu1.nng'
+    },
+    {
+        id:3,
+        name:"María Otero",
+        info:'Lorem ipsum dolor sit',
+        img:'arqu3.nng'
+    },
+]
