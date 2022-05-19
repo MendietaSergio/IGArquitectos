1d8871b4efdbb96b3a8e5d6b7d97f195c318e062
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-05-19 01:48:11 -0300

    Añade useform, formcontact, sweetalert2

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,20 +1,70 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { formContact } from "../../Utils/Validations";
 
 export const Footer = () => {
-    return (
-        <footer>
-            <section className="footer-container">
-                <h2 className="title-footer"></h2>
-                <h3 className="subtitle-footer"></h3>
-                <div className="footer">
-                    <form action="">
-                        <input type="text" name='name' placeholder='Nombre' />
-                        <input type="email" name='mail' placeholder='Email' />
-                        <textarea name="message" cols="30" rows="10" placeholder='Ingrese su mensaje...'></textarea>
-                        <button type='submit' className='btn-submit' >Enviar</button>
-                    </form>
-                </div>
-            </section>
-        </footer>
-    )
-}
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const submit = async (data) => {
+    Swal.fire({
+      position: 'center',
+      icon: 'succes',
+      title: '¡Formulario enviado!',
+      showConfirmButton: false,
+      timer: 3000,
+    });
+    reset();
+  };
+  return (
+    <footer>
+      <section className="footer-container">
+        <h2 className="title-footer"></h2>
+        <h3 className="subtitle-footer"></h3>
+        <div className="footer">
+          <ul className="errors-container">
+            <li>
+              {errors.name ? <small>{errors.email.message}</small> : null}
+            </li>
+            <li>
+              {errors.email ? <small>{errors.email.message}</small> : null}
+            </li>
+            <li>
+              {errors.comment ? <small>{errors.comment.message}</small> : null}
+            </li>
+          </ul>
+          <form onSubmit={handleSubmit(submit)}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Nombre"
+              {...register("name", formContact.name)}
+            />
+            <input
+              type="email"
+              name="mail"
+              placeholder="Email"
+              {...register("email", formContact.email)}
+            />
+
+            <textarea
+              name="comment"
+              cols="30"
+              rows="10"
+              placeholder="Ingrese su mensaje..."
+              {...register("comment", formContact.comment)}
+            ></textarea>
+
+            <button type="submit" className="btn-submit">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </section>
+    </footer>
+  );
+};
