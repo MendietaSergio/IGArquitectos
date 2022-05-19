2af1a82e945b887879e5521119541eb170d2c805
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-05-19 00:23:39 -0300

    Añade importacion de img de los proyectos a mostrar

--- src/Utils/Images.js
+++ src/Utils/Images.js
@@ -3,7 +3,11 @@ import img_03 from '../img/3.png'
 import img_04 from '../img/4.png'
 import img_05 from '../img/5.png'
 
+import proy_01 from '../img/proy1.png'
+import proy_02 from '../img/proy2.png'
+import proy_03 from '../img/proy3.png'
 
 export {
-    img_01, img_03, img_04, img_05
+    img_01, img_03, img_04, img_05,
+    proy_01, proy_02, proy_03
 }
