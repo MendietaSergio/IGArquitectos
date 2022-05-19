f47e5e1ccbdc4a94b9be9f4d7862ab41c33fa7a3
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-05-19 01:11:51 -0300

    Añade url de cada integrante del equipo

--- src/Mock/getInfo.js
+++ src/Mock/getInfo.js
@@ -3,18 +3,18 @@ export const team =[
         id:1,
         name:"Alberto Gomez",
         info:'Lorem ipsum dolor sit',
-        img:'arqu2.nng'
+        img:'https://raw.githubusercontent.com/MendietaSergio/IGArquitectos/master/src/img/arqu2.png'
     },
     {
         id:2,
         name:"Analía Jumenez",
         info:'Lorem ipsum dolor sit',
-        img:'arqu1.nng'
+        img:'https://github.com/MendietaSergio/IGArquitectos/blob/master/src/img/arqu1.png?raw=true'
     },
     {
         id:3,
         name:"María Otero",
         info:'Lorem ipsum dolor sit',
-        img:'arqu3.nng'
+        img:'https://github.com/MendietaSergio/IGArquitectos/blob/master/src/img/arqu3.png?raw=true'
     },
 ]
