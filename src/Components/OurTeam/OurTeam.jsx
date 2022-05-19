407af3cdd8ef13987d54cc22afda30cbc478dbc5
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-05-18 22:56:32 -0300

    Añade componente donde se muestra en equipo de trabajo, falta agregar url de las img

---
+++ src/Components/OurTeam/OurTeam.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { team } from "../../Mock/getInfo";
+
+export const OurTeam = () => {
+  return (
+    <section className="members">
+      <div className="team-container">
+        <h2 className="about-us">Nuestro equipo</h2>
+        <h3 className="slogan">Conoce a nuestro equipo</h3>
+        <div className="cards">
+          {team.map(person => (
+            <div className="card" key={person.id}>
+              <img src='arqu1.png' alt="" />
+              <h4>{person.name}</h4>
+              <p>{person.info}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
